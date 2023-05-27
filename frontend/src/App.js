b73fbccb16332ce54b761c9cb8c21a2309587264
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-05-27 16:44:29 -0400

    initilize

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>
+            Welcome To Proshop
+          </h1>
+        </Container>
+      </main>
+    </>
+  )
+}
+
+export default App
