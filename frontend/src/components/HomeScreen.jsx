30a1e435ab86c70f404536f79ff6ad08a6a4791b
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-05-28 00:20:49 -0400

    Starting List Products

---
+++ frontend/src/components/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1></h1>
+    </>
+  );
+};
+
+export default HomeScreen;
