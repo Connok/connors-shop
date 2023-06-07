17b894e191b8a09c4aad858c166f7ca28ba3dd54
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-06-06 21:56:04 -0400

    get data from data base

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,18 @@ dotenv.config();
 import conectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000;
+import productRouts from './routes/productRoutes.js';
 
 conectDB(); // conect to MongoDB
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-app.get('/api/products', (req, res) => {
-    res.send(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.send(product)
-})
+    res.send('API is running...');
+});
+
+app.use('/api/products', productRouts);
+
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
