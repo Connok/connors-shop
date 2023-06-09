dcf6876c3e29d99627aa87322129c25207c07ea6
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-06-08 20:14:41 -0400

    custom status messages

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,8 +13,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
         return res.json(product);
+    } else {
+        res.status(404).json;
+        throw new Error('Resource not Found');
     }
-    res.status(404).json({ message: 'Product not found' });
+
 }));
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import conectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000;
 import productRouts from './routes/productRoutes.js';
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRouts);
 
-
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
