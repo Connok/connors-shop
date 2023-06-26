0065d07a03c2862825ed591de7f06c2350d4125f
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-06-25 22:09:19 -0400

    end points

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
-import { CreateApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
-import { createApi } from '@reduxjs/toolkit/dist/query';
+
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
