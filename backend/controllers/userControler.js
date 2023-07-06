bb9cf340323480a6d50d676f8447778e6ed456b3
Connor McCarthy <connokxbox@gmail.com>
2023-07-06 12:37:25 -0400

    logout and clear cookie

--- backend/controllers/userControler.js
+++ backend/controllers/userControler.js
@@ -48,7 +48,11 @@ const regesterUser = asyncHandler(async (req, res) => {
 // @route Post /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // @desc Get USer Profile
