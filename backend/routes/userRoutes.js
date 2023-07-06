5ecba5ff04c0689e123e2a4bfb9350171ceecdd3
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-07-05 23:32:04 -0400

    auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,12 +12,13 @@ import {
     getUserById,
     updateUser
 } from "../controllers/userControler.js";
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 
-router.route('/').post(regesterUser).get(getUsers);
+router.route('/').post(regesterUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+router.post('/auth', authUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser);
 
 export default router;
