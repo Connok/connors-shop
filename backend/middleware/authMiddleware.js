1e6751a5fb39b5ce0352b58e5d85fb3e8166de6c
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-07-06 20:55:22 -0400

    user regster

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,7 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.frindById(decoded.userId).select('-password');
+            req.user = await User.findById(decoded.userId).select('-password');
             next();
         } catch (error) {
             console.log(error);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -18,7 +18,10 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 router.route('/').post(regesterUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/auth', authUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser);
 
 export default router;
