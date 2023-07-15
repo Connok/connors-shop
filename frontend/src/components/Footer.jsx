0ca04c154b6cacffdd25b0e3a8c96a0db6d44da6
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-07-14 21:21:13 -0400

    fotter change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-3">
-            <p>ProShop &copy; {currentYear}</p>
+            <p>Connor's Shop &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
