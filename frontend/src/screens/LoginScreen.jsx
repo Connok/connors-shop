04202b702da4eba51ef57987f366d97efdff61d4
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-07-25 21:14:39 -0400

    imports and fixes

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,11 +1,11 @@
-import { USERS_URL_URL } from "../constants";
+import { USERS_URL } from "../constants";
 import { apiSlice } from './apiSlice';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: USER_URL / auth,
+                url: `${USERS_URL} / auth`,
                 method: 'POST',
                 body: data,
             }),
@@ -15,4 +15,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 
 });
 
-export const { useLoginMutation } = usersApiSliceApiSlice;
+export const { useLoginMutation } = usersApiSlice;
