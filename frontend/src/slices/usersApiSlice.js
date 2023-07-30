1608e3ccfc801dbe225bbd7bcbcf6f974b727e5b
connor mccarthy <74563791+Connok@users.noreply.github.com>
2023-07-29 21:39:46 -0400

    logout start

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -10,9 +10,16 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
         }),
+        logout: builder.mutation({
+            query: () => ({
+                url: `${USERS_URL}/logout`,
+                method: 'POST',
+
+            }),
+        })
 
     }),
 
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
