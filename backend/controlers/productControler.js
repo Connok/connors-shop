81a9f5949a064df9d88fd7a7d06242b2c3fae619
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-05 22:37:25 -0500

    missed if statment

--- backend/controlers/productControler.js
+++ backend/controlers/productControler.js
@@ -47,17 +47,23 @@ const createProduct = asyncHandler(async (req, res) => {
 const updateProduct = asyncHandler(async (req, res) => {
     const { name, price, description, image, brand, category, countInStock } = req.body;
     const product = await Product.findById(req.params.id);
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
 
-    product.name = name;
-    product.price = price;
-    product.description = description;
-    product.image = image;
-    product.brand = brand;
-    product.category = category;
-    product.countInStock = countInStock;
+        const updateProduct = await product.save();
+        res.json(updateProduct);
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
 
-    const updateProduct = await product.save();
-    res.json(updateProduct);
+
 });
 
-export { getProducts, getProductById, createProduct };
+export { getProducts, getProductById, createProduct, updateProduct };
