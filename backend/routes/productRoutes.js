72d718b0ebc05610f05775bd42470e28a3781806
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-06 22:47:32 -0500

    update product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 const router = express.Router();
 //import products from '../data/products.js';
-import { getProducts, getProductById, createProduct } from "../controlers/productControler.js";
+import { getProducts, getProductById, createProduct, updateProduct } from "../controlers/productControler.js";
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct);
 
 
 export default router;
