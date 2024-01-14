154cdc026536ae00b7574a674291af9ec0d76219
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-14 16:43:04 -0500

    big paypal fix

--- backend/controlers/orderControler.js
+++ backend/controlers/orderControler.js
@@ -1,30 +1,43 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
+import Product from '../models/productModel.js';
+import { calcPrices } from '../utils/calcPrices.js';
+import { verifyPayPalPayment, checkIfNewTransaction } from '../utils/paypal.js';
 
 // @desc Creat new Orders
 // @route POST /api/orders
 // @access Private 
 const addOrderItems = asyncHandler(async (req, res) => {
-    const {
-        orderItems,
-        shippingAddress,
-        paymentMethod,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-    } = req.body
+    const { orderItems, shippingAddress, paymentMethod } = req.body;
 
     if (orderItems && orderItems.length === 0) {
         res.status(400);
-        throw new Error('No Order items');
+        throw new Error('No order items');
     } else {
+        // get the ordered items from our database
+        const itemsFromDB = await Product.find({
+            _id: { $in: orderItems.map((x) => x._id) },
+        });
+
+        // map over the order items and use the price from our items from database
+        const dbOrderItems = orderItems.map((itemFromClient) => {
+            const matchingItemFromDB = itemsFromDB.find(
+                (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
+            );
+            return {
+                ...itemFromClient,
+                product: itemFromClient._id,
+                price: matchingItemFromDB.price,
+                _id: undefined,
+            };
+        });
+
+        // calculate prices
+        const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
+            calcPrices(dbOrderItems);
+
         const order = new Order({
-            orderItems: orderItems.map((x) => ({
-                ...x,
-                product: x._id,
-                _id: undefined
-            })),
+            orderItems: dbOrderItems,
             user: req.user._id,
             shippingAddress,
             paymentMethod,
@@ -36,10 +49,9 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
         const createdOrder = await order.save();
 
-        res.status(201).json(createdOrder)
+        res.status(201).json(createdOrder);
     }
 });
-
 // @desc Get logged in user Orders
 // @route GET /api/orders/myorders
 // @access Private 
@@ -66,9 +78,20 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route PUT /api/orders/:id/pay
 // @access Private 
 const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const { verified, value } = await verifyPayPalPayment(req.body.id);
+    if (!verified) throw new Error('Payment not verified');
+
+    // check if this transaction has been used before
+    const isNewTransaction = await checkIfNewTransaction(Order, req.body.id);
+    if (!isNewTransaction) throw new Error('Transaction has been used before');
+
     const order = await Order.findById(req.params.id);
 
     if (order) {
+        // check the correct amount was paid
+        const paidCorrectAmount = order.totalPrice.toString() === value;
+        if (!paidCorrectAmount) throw new Error('Incorrect amount paid');
+
         order.isPaid = true;
         order.paidAt = Date.now();
         order.paymentResult = {
@@ -76,15 +99,15 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
             status: req.body.status,
             update_time: req.body.update_time,
             email_address: req.body.payer.email_address,
-        }
-        const updateOrder = await order.save();
+        };
 
-        res.status(200).json(updateOrder);
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
     } else {
         res.status(404);
-        throw new Error('Order Not Found');
+        throw new Error('Order not found');
     }
-
 });
 
 // @desc Update order to delivered
