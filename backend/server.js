ddd4ef1c69da6d19f98a83371b431b45da71e5b1
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-14 15:12:27 -0500

    server side working

--- backend/server.js
+++ backend/server.js
@@ -3,50 +3,43 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
-import conectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-//import products from './data/products.js';
-
+import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT || 5000;
-conectDB(); // conect to MongoDB
+
+connectDB();
 
 const app = express();
 
-// Body Parcer middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Cookie Parser Middleware
 app.use(cookieParser());
-
-
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
-app.get("/api/config/paypal", (req, res) =>
+app.get('/api/config/paypal', (req, res) =>
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve(); // Set __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
 if (process.env.NODE_ENV === 'production') {
-    // set Static folder
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    // any route the is not api will be redirected to index.html
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
-
 } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
     app.get('/', (req, res) => {
         res.send('API is running....');
     });
@@ -55,4 +48,6 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
