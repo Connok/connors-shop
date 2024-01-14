f977b7aee5661381176820e07bb1c909ae588163
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-14 15:36:11 -0500

    image update

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -124,7 +124,7 @@ const ProductEditScreen = () => {
                 type="text"
                 placeholder="Enter Image URL"
                 value={image}
-                onChange={(e) => setImage}
+                onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
               <Form.Control
                 type="file"
