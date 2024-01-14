2e7233b59ec841151b07a0afe42a65a490ae17cf
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-14 13:50:10 -0500

    PRoduction setup

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,21 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+    // set Static folder
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+    // any route the is not api will be redirected to index.html
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
