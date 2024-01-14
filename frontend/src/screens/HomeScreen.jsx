8e08c69033321e5cbf6a56829a0cb02c51fac874
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-13 21:58:14 -0500

    Paginate front end

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate.jsx";
 import { useGetProductsQuery } from "../slices/productsApiSlice.js";
 
 const HomeScreen = () => {
@@ -26,6 +27,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
