562e43579664c1f90a9ef19fd2c982151e961555
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-01-15 21:35:49 -0500

    tital change

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -32,7 +32,7 @@ const HomeScreen = () => {
       ) : (
         <>
           <Meta />
-          <h1>Latest Products</h1>
+          <h1>Connor's Classics</h1>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
